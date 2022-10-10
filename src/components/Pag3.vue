4e3d9d0ce45b44fba50726e1c17fc6aed10336c9
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-10-10 13:33:37 -0300

    add

--- src/components/Pag3.vue
+++ src/components/Pag3.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-white">
-    <div class="flex flex-row bg-red-400">
+    <div class="flex flex-row bg-red-400 flex-wrap">
         <div class="bg-red-600 p-6 m-2 rounded">1</div>
         <div class="bg-red-600 p-6 m-2 rounded">2</div>
         <div class="bg-red-600 p-6 m-2 rounded">3</div>
@@ -10,7 +10,7 @@
         <div class="bg-blue-600 p-6 m-2 rounded text-center">2</div>
         <div class="bg-blue-600 p-6 m-2 rounded text-center">3</div>
     </div>
-    <div class="flex flex-row-reverse bg-green-400 my-2">
+    <div class="flex flex-row-reverse bg-green-400 my-2 flex-wrap">
         <div class="bg-green-600 p-6 m-2 rounded text-center">1</div>
         <div class="bg-green-600 p-6 m-2 rounded text-center">2</div>
         <div class="bg-green-600 p-6 m-2 rounded text-center">3</div>
@@ -20,6 +20,23 @@
         <div class="bg-yellow-600 p-6 m-2 rounded text-center">2</div>
         <div class="bg-yellow-600 p-6 m-2 rounded text-center">3</div>
     </div>
+</div>
+
+<div class="flex flex-col bg-red-900 p-3 text-white text-center my-2">
+    <div class="bg-pink-900 rounded m-2 p-2">1</div>
+    <div class="bg-pink-800 rounded m-2 p-2">2</div>
+    <div class="bg-pink-700 rounded m-2 p-2">3</div>
+    <div class="bg-pink-600 rounded m-2 p-2">4</div>
+    <div class="bg-pink-500 rounded m-2 p-2">5</div>
+    <div class="bg-pink-400 rounded m-2 p-2">6</div>
+</div>
+<div class="flex flex-wrap bg-green-300">
+    <div class="bg-green-900 rounded m-2 p-2">1</div>
+    <div class="bg-green-800 rounded m-2 p-2">2</div>
+    <div class="bg-green-700 rounded m-2 p-2">3</div>
+    <div class="bg-green-600 rounded m-2 p-2">4</div>
+    <div class="bg-green-500 rounded m-2 p-2">5</div>
+    <div class="bg-green-400 rounded m-2 p-2">6</div>
 </div>
 </template>
 <script>
