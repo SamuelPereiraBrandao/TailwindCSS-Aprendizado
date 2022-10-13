34d006ec7f9cf7ab33bf46033e3d8289d167a245
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-10-13 19:25:51 -0300

    add

--- src/components/Pag5.vue
+++ src/components/Pag5.vue
@@ -3,6 +3,46 @@
   <p class="text-2xl tracking-tighter">Espaçamento entre letras MENOR</p>
   <p class="text-2xl tracking-widest">Espaçamento entre letras MAIOR</p>
   <p class="text-2xl w-64 bg-yellow-300 leading-loose rounded">Algumas palavras texto para completar e ver os resultados desse teste que estou fazendo nesse exato momento!</p>
+
+  <div class="bg-red-500 border-2 border-red-900 p-2 mt-2 rounded">
+<p class="text-center text-white font-bold">Alerta!</p>
+  </div>
+
+<p class="text-2xl underline">Este texto vai ter underline</p>
+<p class="text-2xl line-through">Este texto vai ter line trough</p>
+<p class="text-2xl hover:line-through transition-all">Hover trough</p>
+
+<div class="h-48 overflow-auto bg-fixed " style="background-image: url('https://cherry.img.pmdstatic.net/fit/https.3A.2F.2Fimg.2Emaxisciences.2Ecom.2Fs3.2Ffrgsd.2Fmourir-moins-con.2Fdefault_2020-12-15_c33fb12b-1ecf-4e79-af0f-8441a86f65ef.2Ejpeg/1200x675/quality/80/pourquoi-un-coucher-de-soleil-est-il-rouge.jpg')">
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+</div>
+<div class="h-48 overflow-auto bg-local bg-no-repeat" style="background-image: url('https://cherry.img.pmdstatic.net/fit/https.3A.2F.2Fimg.2Emaxisciences.2Ecom.2Fs3.2Ffrgsd.2Fmourir-moins-con.2Fdefault_2020-12-15_c33fb12b-1ecf-4e79-af0f-8441a86f65ef.2Ejpeg/1200x675/quality/80/pourquoi-un-coucher-de-soleil-est-il-rouge.jpg')">
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+</div>
+<p class="p-20 text-white">Testando</p>
+<p class="p-20 text-white">Testando</p>
+
+<div class="h-48 overflow-auto bg-center  bg-no-repeat bg-fixed" style="background-image: url('https://cherry.img.pmdstatic.net/fit/https.3A.2F.2Fimg.2Emaxisciences.2Ecom.2Fs3.2Ffrgsd.2Fmourir-moins-con.2Fdefault_2020-12-15_c33fb12b-1ecf-4e79-af0f-8441a86f65ef.2Ejpeg/1200x675/quality/80/pourquoi-un-coucher-de-soleil-est-il-rouge.jpg')"></div>
+   <p class="p-10 text-white">Testando</p>
+    <p class="p-10 text-white">Testando</p>
+
+
+    <div class="h-48 bg-cover bg-bottom" style="background-image: url('https://cherry.img.pmdstatic.net/fit/https.3A.2F.2Fimg.2Emaxisciences.2Ecom.2Fs3.2Ffrgsd.2Fmourir-moins-con.2Fdefault_2020-12-15_c33fb12b-1ecf-4e79-af0f-8441a86f65ef.2Ejpeg/1200x675/quality/80/pourquoi-un-coucher-de-soleil-est-il-rouge.jpg')">
+
+    </div>
+    <div class="h-48 bg-gradient-to-b from-blue-500 via-green-600 to-yellow-400"></div>
+    <div class="h-48 bg-gradient-to-tr from-gray-900 to-red-600"></div>
+    
+
 </template>
 <script>
 export default {
